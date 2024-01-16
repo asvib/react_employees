f2e6dabf8760c48d15f8b9cb228d83157a014209
Andrei <andreisvib@gmail.com>
2024-01-16 09:18:07 +0300

    properties

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,12 +1,19 @@
 import EmployersListItem from "../employers-list-item/employers-list-item"
 import './employers-list.css';
 
-const EmployersList = () => {
+const EmployersList = ({data}) => {
+    
+
+    const elements = data.map(item => {
+        return (
+            <EmployersListItem {...item}/>//name={item.name} salary={item.salary} 
+        )
+    })        
+
+
     return (
         <ul className="app-list list-group">
-            <EmployersListItem/>
-            <EmployersListItem/>
-            <EmployersListItem/>
+            {elements}
         </ul>
     )
 }
