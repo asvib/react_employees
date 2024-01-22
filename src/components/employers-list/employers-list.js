47d68f4d45db379faa12de6ddea0b40279867207
Andrei <andreisvib@gmail.com>
2024-01-22 21:00:38 +0300

    app-info

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,7 +1,7 @@
 import EmployersListItem from "../employers-list-item/employers-list-item"
 import './employers-list.css';
 
-const EmployersList = ({data, onDelete}) => {
+const EmployersList = ({data, onDelete, onToggleProp}) => {
     
 
     const elements = data.map(item => {
@@ -10,7 +10,9 @@ const EmployersList = ({data, onDelete}) => {
             <EmployersListItem
                 key={id}
                 {...itemProps}
-                onDelete={()=> onDelete(id)}/>//name={item.name} salary={item.salary} 
+                onDelete={()=> onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
+                //onToggleRise={() => onToggleRise(id) }/>//n() => ame={item.name} salary={item.salary} 
         )
     })        
 
